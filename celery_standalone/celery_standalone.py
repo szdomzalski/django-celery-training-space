4206f2c094ac2cfd8afd344dd0dfaa2d3a773748
Szymon Domżalski <100307341+szdomzalski@users.noreply.github.com>
2025-09-24 21:07:49 +0200

    Experimenting with separate queues

--- celery_standalone/celery_standalone.py
+++ celery_standalone/celery_standalone.py
@@ -2,9 +2,10 @@ from celery import Celery
 
 app = Celery('celery_standalone')
 app.config_from_object('celeryconfig')
+app.conf.imports = ('consumer.tasks',)
 app.autodiscover_tasks()
 
 
-@app.task(bind=True, ignore_result=True)
-def debug_task(self) -> None:
-    print(f'Request: {self.request!r}')
+# @app.task(bind=True, ignore_result=True)
+# def debug_task(self) -> None:
+#     print(f'Request: {self.request!r}')
--- demo_celery/demo_celery/celery.py
+++ demo_celery/demo_celery/celery.py
@@ -6,9 +6,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo_celery.settings')
 
 app = Celery('demo_celery')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.task_routes = {'consumer.tasks.add': {'queue': 'queue_add'},
+                        'consumer.tasks.xsum': {'queue': 'queue_add'},
+                        'consumer.tasks.mul': {'queue': 'queue_multiply'},}
 app.autodiscover_tasks()
 
 
-@app.task(bind=True, ignore_result=True)
-def debug_task(self) -> None:
-    print(f'Request: {self.request!r}')
+# @app.task(bind=True, ignore_result=True)
+# def debug_task(self) -> None:
+#     print(f'Request: {self.request!r}')
