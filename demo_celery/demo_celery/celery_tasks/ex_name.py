70c5ca6b48e243d191d9298c8018744cbe12d268
Szymon Domżalski <100307341+szdomzalski@users.noreply.github.com>
2025-10-25 20:05:02 +0200

    Experimenting with exceptions in workers

--- demo_celery/demo_celery/celery_tasks/ex_name.py
+++ demo_celery/demo_celery/celery_tasks/ex_name.py
@@ -1,11 +1,25 @@
 from demo_celery.celery_config import app
+import logging
+
+
+logging.basicConfig(filename='celery_tasks.log', level=logging.ERROR,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
+
+# @app.task(queue='tasks')
+# def my_task_test_1(name: str) -> str:
+#     pass
+
+
+# @app.task(queue='tasks')
+# def my_task_test_2(name: str) -> str:
+#     pass
 
 
 @app.task(queue='tasks')
-def my_task_test_1(name: str) -> str:
-    pass
-
-
-@app.task(queue='tasks')
-def my_task_test_2(name: str) -> str:
-    pass
+def my_task_exception_test():
+    try:
+        raise ConnectionError("Simulated connection error for testing")
+    except Exception as e:
+        logging.error(f"An error occurred in my_task_test_3: {e}")
+        raise  # This will ensure the task is marked as failed in Celery
